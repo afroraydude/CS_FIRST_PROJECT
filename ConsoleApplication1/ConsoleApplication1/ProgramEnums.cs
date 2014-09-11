1ebb615e6919cd6f2ac2589282b4805d12ecf486
afroraydude <rayjo5000@yahoo.com>
2014-09-11 16:43:53 -0400

    fixed some git issues

---
+++ ConsoleApplication1/ConsoleApplication1/ProgramEnums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ProgramEnums
+    {
+
+        public enum Days
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        };
+
+        public enum Months : byte 
+        { 
+            Jan,
+            Feb, 
+            Mar, 
+            Apr, 
+            May, 
+            Jun, 
+            Jul, 
+            Aug, 
+            Sep, 
+            Oct, 
+            Nov, 
+            Dec
+        };
+
+    }
+
+}
---
+++ ConsoleApplication1/ConsoleApplication1/ProgramStructs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ProgramStructs
+    {
+
+        public struct Names
+        {
+            public static string firstName;
+            public static string lastName;
+        }
+
+    }
+
+}
