41bd92462e9af06548e2f632b1d60f3bfb136e15
afroraydude <rayjo5000@yahoo.com>
2015-02-11 13:45:59 -0500

    Update Main.cs

--- ConsoleApplication1/ConsoleApplication1/Main.cs
+++ ConsoleApplication1/ConsoleApplication1/Main.cs
@@ -3,24 +3,30 @@ using System;
 
 namespace AfroDudeCS {
     
-    class CubeVol {
+    public class CubeVol {
         long width;
         long length;
         long height;
         
-        public void getVals() {
+        public void GetVals() {
             Console.Writeline("Give length: ");
             length = Console.ReadLine();
             Console.Writeline("Give width: ");
             width = Console.ReadLine();
             Console.Writeline("Give height: ");
-            h
+            height = Console.ReadLine();
         }
         
         public void GetVolume(long l, long w, long h) {
             return l*w*h;
         } 
-        
+        public void Display() {
+            GetVals();
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Height: {0}", height);
+            Console.WriteLine("Volume: {0}", GetVolume());
+        }
         
     }
     
@@ -28,6 +34,7 @@ namespace AfroDudeCS {
         CubeVol cv = new CubeVol();
         
         static void Main(string[] args) {
+            cv.Display();
             
         }
     }
